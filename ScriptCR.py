bab165fcb70c3033eb5d5e88a69c8b86655dfcba
Gustavo Losch <gustavolosch@icloud.com>
2024-05-09 03:35:44 -0300

    Script Release + Documentation

--- ScriptCR.py
+++ ScriptCR.py
@@ -3,7 +3,7 @@ import pandas
 import pyautogui
 import time
 
-tabela = tabula.read_pdf("Extrato Cecilia.pdf", pages="all")[0]
+tabela = tabula.read_pdf("Path.pdf", pages="all")[0] #PDF Table read and transform to dataframe
 tabela.rename(columns=tabela.iloc[4], inplace = True)
 tabela = tabela[['DOCTO:', 'VENCIMENTO:', 'R$ DEVIDO:']]
 tabela = tabela.drop(0)
@@ -13,17 +13,18 @@ tabela = tabela.drop(3)
 tabela = tabela.drop(4)
 
 
-pyautogui.click(1239,1051)
+pyautogui.click(1239,1051) #Preparing CIAF to recieve the commands
 pyautogui.click(23,251)
 time.sleep(1)
 
 
-os = tabela["DOCTO:"].values
+os = tabela["DOCTO:"].values #Transforming dataframe to array
 preco = tabela["R$ DEVIDO:"].values
 cont = 0
 
 for nos in os:
-    pyautogui.click(1382,537)
+
+    pyautogui.click(1382,537) #Sequence of clicks
     time.sleep(0.5)
     pyautogui.typewrite(nos)
     time.sleep(0.5)
@@ -34,9 +35,11 @@ for nos in os:
     time.sleep(0.5)
     pyautogui.press('enter')
     time.sleep(0.5)
-    pyautogui.click(1380,598)
+    pyautogui.click(1381,598)
     time.sleep(0.5)
-    pyautogui.click(991,592)
+    pyautogui.press('tab')
+    time.sleep(0.5)
+    pyautogui.press('enter')
     time.sleep(0.5)
     pyautogui.press('tab')
     time.sleep(0.5)
@@ -58,11 +61,11 @@ for nos in os:
     time.sleep(0.5)
     pyautogui.click(953,590)
     time.sleep(1)
-    time.sleep(1)
 
     print(nos)
     print(tabela.iloc[cont,2])
-    cont = cont + 1
+
+    cont = cont + 1 #Line skip
 
     
 
