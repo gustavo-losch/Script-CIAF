55bf218095ca0d681b4ab635bf0b35e367f18c86
Gustavo Losch <gustavolosch@icloud.com>
2024-05-14 12:45:07 -0300

    VL_TOTAL added to dbf_extractos.py

--- Conversão/dbf_extractor.py
+++ Conversão/dbf_extractor.py
@@ -2,11 +2,11 @@ from dbfread import DBF
 import pandas as pd
 import openpyxl
 
-dbf = DBF('ORDEM-SERV.DBF')
+dbf = DBF(r'C:\Users\Oficina\Desktop\Gustavo\Repositórios\Script-CIAF\Conversão\ORDEM-SERV.DBF')
 dataResult = pd.DataFrame(iter(dbf))
-tabela = dataResult[['NR_ORDEM','NRSERIE', 'DATAFECHA', 'CODCLI', 'NOMECLIENT']]
+tabela = dataResult[['NR_ORDEM','NRSERIE', 'DATAFECHA', 'VL_TOTAL', 'CODCLI', 'NOMECLIENT']]
 df = tabela[tabela['CODCLI'].str.contains('147')]
-df['DATAFECHA'] = pd.to_datetime(df['DATAFECHA'], errors='coerce')
+df.loc[:, 'DATAFECHA'] = pd.to_datetime(df['DATAFECHA'], errors='coerce')
 
 datai = input()
 dataf = input()
@@ -16,4 +16,4 @@ data_final = pd.to_datetime(dataf)
 
 df_filtrado = df[(df['DATAFECHA'] >= data_inicial) & (df['DATAFECHA'] <= data_final)]
 
-df_filtrado.to_excel("Tabela Bergerson.xlsx")
+df_filtrado.to_excel("Tabela Bergerson2.xlsx")
